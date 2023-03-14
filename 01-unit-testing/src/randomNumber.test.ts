239e240ec73d8c5dfd84b9b0fcd4a2a6bdb752aa
Johan Nordström <johan@digitalvillage.se>
2023-03-14 11:56:19 +0100

    Test random number generator.

--- 01-unit-testing/src/randomNumber.test.ts
+++ 01-unit-testing/src/randomNumber.test.ts
@@ -3,4 +3,24 @@ import { getRandomNumber } from './randomNumber'
 
 test('generates a random number between 1-10', () => {
 	const randomNumber = getRandomNumber()
+
+	expect(randomNumber).toBeGreaterThanOrEqual(1)   // assertion
+	expect(randomNumber).toBeLessThanOrEqual(10)
+})
+
+test('generates a random number between 1-50', () => {
+	const max = 50
+	const randomNumber = getRandomNumber(max)
+
+	expect(randomNumber).toBeGreaterThanOrEqual(1)   // assertion
+	expect(randomNumber).toBeLessThanOrEqual(max)
+})
+
+test('generates 10 random numbers between 1-10', () => {
+	for (let i = 0; i < 10; i++) {
+		const randomNumber = getRandomNumber()
+
+		expect(randomNumber).toBeGreaterThanOrEqual(1)   // assertion
+		expect(randomNumber).toBeLessThanOrEqual(10)
+	}
 })
