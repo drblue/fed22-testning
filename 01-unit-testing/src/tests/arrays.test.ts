02382c50aefc3a5204022a04f8b7bc6e8ac805ca
Johan Nordström <johan@digitalvillage.se>
2023-03-14 14:15:22 +0100

    Test array cloning.

---
+++ 01-unit-testing/src/tests/arrays.test.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect, beforeAll } from 'vitest'
+import { clone } from '../utils/arrays'
+
+describe('clones an array', () => {
+	const a = ['i', 'like', 'unit', 'tests']
+	let b: Array<any> = []
+
+	beforeAll(() => {
+		b = clone(a)
+	})
+
+	// test that cloned array contains the same number of items
+	it('contains the same number of items', () => {
+		expect(b.length).toBe(a.length)
+	})
+
+	// test that cloned array contains the same items
+	it('contains the same items', () => {
+		expect(b).toEqual(a)
+	})
+
+	// test that cloned array is *NOT* the same (as the original array)
+	it('is not the same array', () => {
+		expect(b).not.toBe(a)
+	})
+})
---
+++ 01-unit-testing/src/utils/arrays.ts
@@ -0,0 +1,9 @@
+/**
+ * Array utils
+ */
+
+// Clone array (4 realz home-boi)
+export const clone = (arr: Array<any>) => {
+	// return arr
+	return [...arr]
+}
