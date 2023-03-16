dbeeaa6d8928d02114329ee67918c15aca4e2929
Johan Nordström <johan@digitalvillage.se>
2023-03-16 12:09:23 +0100

    WIP unit-testing of todoStorage.

--- 02-spying/src/tests/todoStorage.test.ts
+++ 02-spying/src/tests/todoStorage.test.ts
@@ -1,12 +1,36 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it } from 'vitest'
+import mockLocalStorage from '../mocks/mockedLocalStorage'
 import { getTodos, saveTodos } from '../utils/todoStorage'
+import { Todo } from '../types/Todo'
+
+global.localStorage = mockLocalStorage()
+
+const TODO: Todo = {
+	id: 1,
+	title: 'My first todo',
+	completed: false,
+}
 
 describe('get todos', () => {
-	it.todo('returns empty list of todos', () => {})
+	it('returns empty list of todos', () => {
+		const todos = getTodos()
+
+		expect(todos.length).toBe(0)
+	})
 })
 
 describe('save todos', () => {
-	it.todo('can save a todo', () => {})
+	afterEach(() => {
+		global.localStorage.clear()
+	})
 
-	it.todo('can save a todo and then get it', () => {})
+	it('can save a todo', () => {
+		const result = saveTodos( [ TODO ] )
+
+		expect(result.success).toBe(true)
+	})
+
+	it.todo('can save a todo and then get it', () => {
+		// localStorage will be empty
+	})
 })
