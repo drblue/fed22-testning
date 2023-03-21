a9f630d97b2cdffc8d6a5c7cf889a802a7808ed9
Johan Nordström <johan@digitalvillage.se>
2023-03-21 15:21:20 +0100

    Test that a todo can be created and then retrieved.

--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -25,7 +25,16 @@ describe('TodoAPI', () => {
 		})
 	})
 
-	it.todo('should create and then get the todo')
+	it('should create and then get the todo', async () => {
+		// create a new todo
+		const createdTodo = await TodoAPI.createTodo(newTodo)
+
+		// try to get the todo
+		const todo = await TodoAPI.getTodo(createdTodo.id)
+
+		// expect both todos to have the same key/values
+		expect(todo).toStrictEqual(createdTodo)
+	})
 
 	it.todo('should create and then find the todo among all todo')
 
