e2b6504a0ee50c281caf12b878704c746b6407b0
Johan Nordström <johan@digitalvillage.se>
2023-03-24 14:29:38 +0100

    Group tests under a context.

--- 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -3,23 +3,25 @@ describe('Home Page', () => {
 		cy.visit('http://localhost:3000')
 	})
 
-	it('the h1 contains the correct text', () => {
-		cy.get('[data-test="hero-heading"]')
-			.should('exist')
-			.contains('Testing Next.js Applications with Cypress')
-	})
+	context('Hero section', () => {
+		it('the h1 contains the correct text', () => {
+			cy.get('[data-test="hero-heading"]')
+				.should('exist')
+				.contains('Testing Next.js Applications with Cypress')
+		})
 
-	it('the features on the homepage are correct', () => {
-		cy.get('dt')
-			.eq(0)
-			.contains('4 Courses')
+		it('the features on the homepage are correct', () => {
+			cy.get('dt')
+				.eq(0)
+				.contains('4 Courses')
 
-		cy.get('dt')
-			.eq(1)
-			.contains('25+ Lessons')
+			cy.get('dt')
+				.eq(1)
+				.contains('25+ Lessons')
 
-		cy.get('dt')
-			.eq(2)
-			.contains('Free and Open Source')
+			cy.get('dt')
+				.eq(2)
+				.contains('Free and Open Source')
+		})
 	})
 })
