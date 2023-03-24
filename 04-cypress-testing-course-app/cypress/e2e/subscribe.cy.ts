951423b8fdc50e1d378a5205ff7eb9452c373a3e
Johan Nordström <johan@digitalvillage.se>
2023-03-24 14:19:57 +0100

    Added sad path for invalid email, already subscribed email and no email.

--- 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -16,7 +16,21 @@ describe('Newsletter Subscribe', () => {
 		cy.getByDataTest('success-message').should('exist').contains(email)
 	})
 
-	it.skip('displays an error message when the email is invalid')
+	it('displays an error message when the email is invalid', () => {
+		cy.getByDataTest('email-input').type(invalidEmail)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('success-message').should('not.exist')
+		cy.getByDataTest('server-error-message').should('not.exist')
+	})
 
-	it.skip('should not allow users to subscribe twice')
+	it('should not allow users to subscribe twice', () => {
+		cy.getByDataTest('email-input').type(subscribedEmail)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('server-error-message').should('exist').contains(subscribedEmail).contains('already exists')
+	})
+
+	it('should not allow subscribing without an email address', () => {
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('error-message').should('exist').contains('Email is required')
+	})
 })
