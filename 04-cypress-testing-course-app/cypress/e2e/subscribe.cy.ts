a58e45d596dfd11a4372ab6a9ee1d828dc76ea98
Johan Nordström <johan@digitalvillage.se>
2023-03-24 13:36:17 +0100

    Added faker to fake various data.

--- 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -1,12 +1,18 @@
+import { faker } from '@faker-js/faker'
+
 describe('Newsletter Subscribe', () => {
+	const email = faker.internet.email()
+	const invalidEmail = 'pelle'
+	const subscribedEmail = 'john@example.com'
+
 	beforeEach(() => {
 		cy.visit('http://localhost:3000')
 	})
 
 	it('allows users to subscribe to the email list', () => {
 		// cy.get('[data-test="email-input"]')
-		cy.getByDataTest('email-input').type('pelle@svanslos.nu')
+		cy.getByDataTest('email-input').type(email)
 		cy.getByDataTest('submit-button').click()
-		cy.getByDataTest('success-message').should('exist').contains('pelle@svanslos.nu')
+		cy.getByDataTest('success-message').should('exist').contains(email)
 	})
 })
